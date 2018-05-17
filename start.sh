9504f333009e97a6c23cb3388ce8367d65d2c27a
Ubuntu <ubuntu@ip-172-31-26-34.eu-west-1.compute.internal>
2018-05-17 21:45:28 +0000

    keep only selenoid services

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 
 # pull required docker images
-docker pull selenoid/vnc:chrome_64.0
-docker pull selenoid/vnc:chrome_63.0
+docker pull selenoid/vnc:chrome_66.0
+docker pull selenoid/vnc:chrome_65.0
+docker pull selenoid/vnc:firefox_59.0
 docker pull selenoid/vnc:firefox_58.0
-docker pull selenoid/vnc:firefox_57.0
 
 #-------------- START EVERYTHING ------------------------------
 docker-compose up -d
